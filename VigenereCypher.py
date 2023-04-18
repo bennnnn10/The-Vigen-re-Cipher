e8c9b21324ac6368463349f09a930fbb8574a7e1
unknown <rv8828120@gmail.com>
2023-04-18 20:30:35 +0800

    Pseudocode

---
+++ VigenereCypher.py
@@ -0,0 +1,11 @@
+#Vargas, Ruben A.
+#BSCPE 1-4
+#Object-Oriented Programming
+
+#Ask the name of the user
+#Create a python program using Vigenère Cipher to encrypt the message
+#Convertion of message and key to their specific numerical numbers
+#Encrypt the message using Vigenère Cipher
+#Ciphertext numerical numbers to letters
+#Ask the user to enter the message and key
+#Print the outcome
