ef010fff4783bafb7ff1a0c767833fb39e1c411e
unknown <rv8828120@gmail.com>
2023-04-18 20:36:12 +0800

    User's Information

--- VigenereCypher.py
+++ VigenereCypher.py
@@ -3,6 +3,10 @@
 #Object-Oriented Programming
 
 #Ask the name of the user
+user_name = input(f"\nWhat is your name? ")
+print(f"\nHey there, " + user_name + "!")
+print("Please enter below the text that you want to encrypt!")
+
 #Create a python program using Vigenère Cipher to encrypt the message
 #Convertion of message and key to their specific numerical numbers
 #Encrypt the message using Vigenère Cipher
